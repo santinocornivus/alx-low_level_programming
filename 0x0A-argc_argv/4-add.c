1d1f60d167dba494143c214e8c677e240f972036
santinocornivus <santinocornivus@gmail.com>
2022-09-30 23:33:07 +0200

    adds positve numbers passed from CLI

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,34 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
+#include "main.h"
 
 /**
- * main - adds positive numbers passed to it
- * @argc: argument count
- * @argv: argument array
- *
- * Return: 1 if a non-integer is among the passed arguments, 0 otherwise
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ *  @argv: Array name
+ *Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
-
 int main(int argc, char *argv[])
 {
-	int i, sum;
-	long unsigned int j;
+	int i, j, length, sum;
+	char *ptr;
 
 	if (argc < 2)
-	{
 		printf("0\n");
-	}
 	else
 	{
 		sum = 0;
+
 		for (i = 1; i < argc; i++)
 		{
-			for (j = 0; j < strlen(argv[i]); j++)
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
 			{
-				if (0 == isdigit(argv[i][j]))
+				if (isdigit(*(ptr + j)) == 0)
 				{
 					printf("Error\n");
 					return (1);
