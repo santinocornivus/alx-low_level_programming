8fd8e610a1b858ce534b61884f8db56884b4b32a
santinocornivus <santinocornivus@gmail.com>
2022-10-21 06:20:35 +0200

    updated print_list func

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,15 +13,17 @@ size_t print_list(const list_t *h)
 {
 	unsigned int index;
 
-	if ((*h).str == NULL)
-	{
-		printf("[%d] %s\n", 0, "(nil)");
-	}
-
 	index = 0;
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if ((*h).str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		} else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+
 		h = h->next;
 		index++;
 	}
